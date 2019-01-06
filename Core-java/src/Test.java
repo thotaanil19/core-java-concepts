75de28eb2c981126ee8c70b409509aa8e1ab166d
Anil <thota.anil19@gmail.com>
2019-01-06 11:18:54 -0800

    sum method

--- Core-java/src/Test.java
+++ Core-java/src/Test.java
@@ -2,7 +2,12 @@
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println(" Anil".startsWith(""));
+		System.out.println(sum(10, 20));
 	}
+	
+	public static int sum (int a, int b) {
+		return a+b;
+	}
+	
 
 }
