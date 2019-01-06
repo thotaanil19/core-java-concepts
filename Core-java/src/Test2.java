81f7e2b0540a2c3bf2df677f820780544db1338a
Anil <thota.anil19@gmail.com>
2019-01-06 11:45:51 -0800

    test2

---
+++ Core-java/src/Test2.java
@@ -0,0 +1,14 @@
+
+public class Test2 {  
+	
+	
+	public static void main(String[] args) {
+		System.out.println(sum(10, 20));
+	}
+	
+	public static int sum (int a, int b) {
+		return a+b;
+	}
+	
+
+}
